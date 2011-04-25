b98937e6e1c0dbb7613b0ec61eb4c2f602a833d7
Alexander Færøy <ahf@0x90.dk>
2011-04-25 20:42:34 +0200

    Add mkpasswd utility.
    Currently supports MD5, SHA1, SHA256 and SHA512.

--- Makefile
+++ Makefile
@@ -1,7 +1,9 @@
 all:
 	$(MAKE) -C src/
+	$(MAKE) -C utils/
 
 clean:
 	$(MAKE) -C src/ clean
+	$(MAKE) -C utils/ clean
 
 .phony: clean all
---
+++ utils/novo-mkpasswd/mkpasswd.go
@@ -0,0 +1,60 @@
+/* vim: set sw=4 sts=4 et foldmethod=syntax : */
+
+/*
+ * Copyright (c) 2011 Alexander Færøy <ahf@0x90.dk>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package main
+
+import (
+    "bufio"
+    "crypto/md5"
+    "crypto/sha1"
+    "crypto/sha256"
+    "crypto/sha512"
+    "fmt"
+    "hash"
+    "os"
+)
+
+func F(h hash.Hash, name, str string) {
+    h.Write([]byte(str))
+    fmt.Printf("%s\t: '%x'\n", name, h.Sum())
+}
+
+func main() {
+    fmt.Printf("Password: ");
+    stdin := bufio.NewReader(os.Stdin);
+    input, error := stdin.ReadString('\n');
+
+    if error != nil {
+        os.Exit(1)
+    }
+
+    F(md5.New(), "MD5", input)
+    F(sha1.New(), "SHA1", input)
+    F(sha256.New(), "SHA256", input)
+    F(sha512.New(), "SHA512", input)
+}
