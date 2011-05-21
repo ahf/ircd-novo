c17d3ba4d89430e6d813ba89fe66b33e7fec8905
Alexander Færøy <ahf@0x90.dk>
2011-05-21 20:33:31 +0200

    Kill ClientConnectionHandler.

--- src/listeners.go
+++ src/listeners.go
@@ -122,7 +122,6 @@ func (this *BasicListener) Listen() {
 
 func (this *BasicListener) ConnectionHandler(ircd *Ircd, connection net.Conn, remoteAddr string) {
     this.Printf("Incoming Connection from %s to %s (%s %s)", remoteAddr, this.Address(), this.Secure(), this.Protocol())
-    ClientConnectionHandler(ircd, connection, remoteAddr)
 }
 
 type TCPListener struct {
